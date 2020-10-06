775d255618a3bedd6d2409aeb54b0fc9d230a053
R2DToo <bradenxd12@gmail.com>
2020-10-05 21:13:23 -0500

    Created seed script for Generations, Pokemon Species, and Pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,39 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+Pokemon.delete_all
+PokemonSpecy.delete_all
+Generation.delete_all
+Pokedex.delete_all
+PokedexEntry.delete_all
+Type.delete_all
+PokemonType.delete_all
+
+all_generations = PokeApi.get(:generation)
+
+all_generations.count.times do |i|
+  i += 1
+  api_generation = PokeApi.get(generation: i)
+  new_generation = Generation.create(id: i, name: api_generation.name, region: api_generation.main_region.name)
+  api_generation.pokemon_species.each do |species|
+    new_species = new_generation.pokemon_species.create(id: species.url[42..-2])
+  end
+end
+
+all_pokemon = PokeApi.get(:pokemon)
+
+offset = 0
+page_item_limit = 20
+total_pages = all_pokemon.count / page_item_limit
+
+total_pages.times do
+  pokemon_page = PokeApi.get(pokemon: { limit: page_item_limit, offset: offset })
+  pokemon_page.results.each do |basic_pokemon|
+    pokemon_id = basic_pokemon.url[34..-2]
+    api_pokemon = PokeApi.get(pokemon: pokemon_id)
+    new_pokemon = Pokemon.create(id: api_pokemon.id, name: api_pokemon.name, height: api_pokemon.height, weight: api_pokemon.weight, pokemon_specy_id: api_pokemon.species.url[42..-2])
+    puts "ID: #{new_pokemon.id} Name: #{new_pokemon.name} Height: #{new_pokemon.height} Weight: #{new_pokemon.weight} SpeciesID: #{new_pokemon.pokemon_specy_id}"
+  end
+  offset += 20
+end
