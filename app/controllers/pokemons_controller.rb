df20b2391365516221c9719d7df9caeefbf2600b
R2DToo <bradenxd12@gmail.com>
2020-10-11 12:14:32 -0500

    Added basic content and linkes to pokemons controller and views

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,9 @@
 class PokemonsController < ApplicationController
   def index
+    @pokemons = Pokemon.order("id")
   end
 
   def show
+    @pokemon = Pokemon.includes(:pokemon_types).includes(:types).find(params[:id])
   end
 end
