dc6e0ea3720d54098540d0e7944f55f1516535c0
R2DToo <bradenxd12@gmail.com>
2020-10-10 23:04:03 -0500

    Added basic content to types controller and views

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,7 +1,9 @@
 class TypesController < ApplicationController
   def index
+    @types = Type.all.order("id")
   end
 
   def show
+    @type = Type.includes(:pokemon_types).includes(:pokemons).find(params[:id])
   end
 end
