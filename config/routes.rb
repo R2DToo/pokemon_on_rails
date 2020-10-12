61fd00bccd568968582c1c1b31cd202b6c4a3621
R2DToo <bradenxd12@gmail.com>
2020-10-12 11:58:57 -0500

    Generated Pokemon_Species controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pokemon_species/index'
+  get 'pokemon_species/show'
   resources :types, only: %i[index show]
   resources :pokemons, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
