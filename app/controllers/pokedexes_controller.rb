8660de3d88f525b6b04293aeab435084afce00f2
R2DToo <bradenxd12@gmail.com>
2020-10-12 14:09:03 -0500

    Generated Pokedexes controller

---
+++ app/controllers/pokedexes_controller.rb
@@ -0,0 +1,7 @@
+class PokedexesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :pokemons, only: %i[index show]
   resources :pokemon_species, only: %i[index show]
   resources :generations, only: %i[index show]
+  resources :pokedexes, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
