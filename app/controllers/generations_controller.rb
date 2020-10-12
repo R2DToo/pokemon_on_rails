bc9c039250f63f5df359e632dbb9d214e0ae7ff6
R2DToo <bradenxd12@gmail.com>
2020-10-12 14:07:17 -0500

    Added basic content to generations controller and views

--- app/controllers/generations_controller.rb
+++ app/controllers/generations_controller.rb
@@ -1,7 +1,9 @@
 class GenerationsController < ApplicationController
   def index
+    @generations = Generation.order("id")
   end
 
   def show
+    @generation = Generation.includes(:pokemon_species).find(params[:id])
   end
 end
