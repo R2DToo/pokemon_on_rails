8f0646658a66f8ab21c9599f9e3665d1909f26e1
R2DToo <bradenxd12@gmail.com>
2020-10-12 13:53:58 -0500

    Added content and links to Pokemon_Species Controller and Views

--- app/controllers/pokemon_species_controller.rb
+++ app/controllers/pokemon_species_controller.rb
@@ -1,7 +1,9 @@
 class PokemonSpeciesController < ApplicationController
   def index
+    @pokemon_species = PokemonSpecy.order("id")
   end
 
   def show
+    @pokemon_specy = PokemonSpecy.includes(:pokemons).find(params[:id])
   end
 end
