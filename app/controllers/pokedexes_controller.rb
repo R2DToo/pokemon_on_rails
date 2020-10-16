a2de45318790a6dd50e24f4539078e4cd2d7c3a4
R2DToo <bradenxd12@gmail.com>
2020-10-16 15:31:06 -0500

    Added content to pokedexes model, view, and controller

--- app/controllers/pokedexes_controller.rb
+++ app/controllers/pokedexes_controller.rb
@@ -1,7 +1,14 @@
 class PokedexesController < ApplicationController
   def index
+    @pokedexes = Pokedex.order("id")
   end
 
   def show
+    @pokedex = Pokedex.includes(:pokedex_entries)
+                      .includes(:pokemon_species)
+                      .includes(:pokemons)
+                      .includes(:pokemon_types)
+                      .includes(:types)
+                      .find(params[:id])
   end
 end
--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -1,6 +1,9 @@
 class Pokedex < ApplicationRecord
   has_many :pokedex_entries
   has_many :pokemon_species, through: :pokedex_entries
+  has_many :pokemons, through: :pokemon_species
+  has_many :pokemon_types, through: :pokemons
+  has_many :types, through: :pokemon_types
 
   validates :name, :id, presence: true
   validates :name, length: { minimum: 3 }
