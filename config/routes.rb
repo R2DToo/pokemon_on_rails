37874116cb698fef7a0ce5e3b4c8f22348945fe4
R2DToo <bradenxd12@gmail.com>
2020-10-23 19:29:56 -0500

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :pokemon_species, only: %i[index show]
   resources :generations, only: %i[index show]
   resources :pokedexes, only: %i[index show]
+  resources :home, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
